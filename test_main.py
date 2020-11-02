b110d50c018016dbd8499337f9e1dcb4f4655c33
Jayaraj J <jayarajevur@gmail.com>
2020-11-02 09:02:45 +0530

    reverted back to successful test

--- test_main.py
+++ test_main.py
@@ -28,7 +28,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'not healthy'
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
